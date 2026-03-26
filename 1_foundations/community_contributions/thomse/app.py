7b3355c2a6781006ffd81a79dc5c4fbdd49bf0d9
Thomas Juma <Thomas Juma>
2026-03-26 14:52:14 +0300

    Add the evaluator.

--- 1_foundations/community_contributions/thomse/app.py
+++ 1_foundations/community_contributions/thomse/app.py
@@ -4,12 +4,15 @@ import json
 import os
 import requests
 from pypdf import PdfReader
+from pydantic import BaseModel
+
 import gradio as gr
 
 
 load_dotenv(override=True)
 
 MODEL = "gpt-4o-mini"
+EVALUATOR_MODEL = "google/gemini-2.5-flash-preview-05-20"
 
 openai_api_key = os.getenv('OPENAI_API_KEY')
 
@@ -85,6 +88,38 @@ tools_functions = {
 }
 
 
+class Evaluation(BaseModel):
+    is_acceptable: bool
+    feedback: str
+
+class Evaluator:
+    def __init__(self):
+        self.gemini = OpenAI(base_url=OPENROUTER_BASE_URL, api_key=os.getenv('OPENROUTER_API_KEY'))
+        self.name = "Thomas Juma"
+    
+    def evaluator_system_prompt(self, name, summary, linkedin):
+        evaluator_system_prompt = f"You are an evaluator that decides whether a response to a question is acceptable. \
+            You are provided with a conversation between a User and an Agent. Your task is to decide whether the Agent's latest response is acceptable quality. \
+            The Agent is playing the role of {name} and is representing {name} on their website. \
+            The Agent has been instructed to be professional and engaging, as if talking to a potential client or future employer who came across the website. \
+            The Agent has been provided with context on {name} in the form of their summary and LinkedIn details. Here's the information:"
+
+        evaluator_system_prompt += f"\n\n## Summary:\n{summary}\n\n## LinkedIn Profile:\n{linkedin}\n\n"
+        evaluator_system_prompt += f"With this context, please evaluate the latest response, replying with whether the response is acceptable and your feedback."
+        return evaluator_system_prompt
+
+    def evaluator_user_prompt(self, reply, message, history):
+        user_prompt = f"Here's the conversation between the User and the Agent: \n\n{history}\n\n"
+        user_prompt += f"Here's the latest message from the User: \n\n{message}\n\n"
+        user_prompt += f"Here's the latest response from the Agent: \n\n{reply}\n\n"
+        user_prompt += "Please evaluate the response, replying with whether it is acceptable and your feedback."
+        return user_prompt
+    
+    def evaluate(self, reply, message, history):
+        messages = [{"role": "system", "content": self.evaluator_system_prompt(self.name, self.summary, self.linkedin)}] + [{"role": "user", "content": self.evaluator_user_prompt(reply, message, history)}]
+        response = self.openai.chat.completions.create(model=EVALUATOR_MODEL, messages=messages, response_format=Evaluation)
+        return response.choices[0].message.content
+
 class Me:
 
     def __init__(self):
@@ -139,8 +174,31 @@ If the user is engaging in discussion, try to steer them towards getting in touc
                 done = True
         return response.choices[0].message.content
     
+    def rerun(self, reply, message, history, feedback):
+        updated_system_prompt = self.system_prompt() + "\n\n## Previous answer rejected\nYou just tried to reply, but the quality control rejected your reply\n"
+        updated_system_prompt += f"## Your attempted answer:\n{reply}\n\n"
+        updated_system_prompt += f"## Reason for rejection:\n{feedback}\n\n"
+        messages = [{"role": "system", "content": updated_system_prompt}] + history + [{"role": "user", "content": message}]
+        response = self.openai.chat.completions.create(model=MODEL, messages=messages)
+        return response.choices[0].message.content
+
+evaluator = Evaluator()
+me = Me()
+
+def chat(message, history):
+    reply = me.chat(message, history)
+
+    evaluation = evaluator.evaluate(reply, message, history)
+    
+    if evaluation.is_acceptable:
+        print("Passed evaluation - returning reply")
+    else:
+        print("Failed evaluation - retrying")
+        print(evaluation.feedback)
+        reply = me.rerun(reply, message, history, evaluation.feedback)       
+    return reply
 
 if __name__ == "__main__":
     me = Me()
-    gr.ChatInterface(me.chat, type="messages").launch()
+    gr.ChatInterface(chat, type="messages").launch()
     
